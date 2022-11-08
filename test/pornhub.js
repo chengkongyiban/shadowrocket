8fa7fedfc7dfc33c6033eb33a687cf1817552bbe
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2022-11-08 14:12:56 +0800

    Delete pornhub.js

--- test/pornhub.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/test/pornhub.css" type="text/css">');
-$done({ body });
