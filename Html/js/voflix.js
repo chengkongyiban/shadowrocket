f393d3d9ca72abecb0e5f05f9deb39a4225b3142
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2022-11-08 14:22:09 +0800

    Update voflix.js

--- Html/js/voflix.js
+++ Html/js/voflix.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/Html/css/voflix.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/chengkongyiban/shadowrocket@main/Html/css/voflix.css" type="text/css">');
 $done({ body });
