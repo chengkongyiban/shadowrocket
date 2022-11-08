a10ae4e034f0bd77832d3bd3c247c0e0d8fa1ab8
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2022-11-08 15:11:59 +0800

    Delete Html directory

--- Html/js/voflix.js
+++
@@ -1,3 +0,0 @@
-var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/chengkongyiban/shadowrocket@main/Html/css/voflix.css" type="text/css">');
-$done({ body });
