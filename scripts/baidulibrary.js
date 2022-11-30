09fe27340c0cafb2d863bb73562b15e0295e1f83
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2022-11-30 20:15:09 +0800

    Create baidulibrary.js

---
+++ scripts/baidulibrary.js
@@ -0,0 +1,14 @@
+[rewrite_local]
+
+# ～ 百度文库☆解锁VIP文档阅读权限（2022-10-17）@ddgksf2013
+^https:\/\/appwk\.baidu\.com\/naapi\/user\/getinfo url script-response-body https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Crack/baidulibrary.js
+
+[mitm]
+
+hostname=appwk.baidu.com
+
+***********************************/
+
+let ddgksf2013 = JSON.parse($response.body);
+ddgksf2013.data.vip.base_vip_info={"uid":12345678,"type":2,"start_time":1622222200,"end_time":4622222200,"is_vip":1,"remain_day":999,"pro_total":5,"normal_total":5};
+$done({ body: JSON.stringify(ddgksf2013) });
