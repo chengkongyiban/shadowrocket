2290b575d1bdcfeb1e07c018824832099b1ab658
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2022-12-29 20:50:39 +0800

    更新 QX_to_Shadowrocket.js

--- scripts/QX_to_Shadowrocket.js
+++ scripts/QX_to_Shadowrocket.js
@@ -9,7 +9,7 @@ hostname = %APPEND% github.com:443, raw.githubusercontent.com:443
 */
 
 let req = $request.url.replace(/qx$/,'')
-let name = '#!name= ' + req.match(/.+\/(.+)\.(conf|js)/)?.[1] || '无名'
+let name = '#!name= ' + req.match(/.+\/(.+)\.(conf|js)/)?.[1] || '无名';
 !(async () => {
   let body = await http(req);
 
@@ -23,16 +23,18 @@ let MITM = "";
 
 body.forEach((x, y, z) => {
 	let type = x.match(
-		/script-|enabled=|reject|echo-response|url\srequest-header|hostname|url\s(302|307)|\s(request|response)-body/
+		/script-|enabled=|reject|echo-response|\-header|hostname|url\s(302|307)|\s(request|response)-body/
 	)?.[0];
 	if (type) {
 		switch (type) {
 			case "script-":
+			if (x.match('echo')) {throw '脚本不支持通用'}
 				z[y - 1]?.match("#") && script.push(z[y - 1]);
+				let proto = x.match('proto.js') ? ',binary-body-mode=1' : '';
 				script.push(
 					x.replace(
 						/(\^?http[^\s]+)\surl\sscript-(response|request)[^\s]+\s(http.+\/(.+)\.js)/,
-						'$4 = type=http-$2,pattern=$1,requires-body=1,max-size=0,script-path=$3,script-update-interval=0',
+						`$4 = type=http-$2,pattern=$1,requires-body=1${proto},max-size=0,script-path=$3,script-update-interval=0`,
 					),
 				);
 				break;
@@ -55,14 +57,24 @@ body.forEach((x, y, z) => {
 				URLRewrite.push(x.replace(/(.*?)\x20url\x20(reject-200|reject-img|reject-dict|reject-array|reject)/, "$1 - $2"));
 				break;
 
-			case "url-and-header":
-				z[y - 1]?.match("#") && HeaderRewrite.push(z[y - 1]);
+			case "-header":
+			if (x.match(/\(\\r\\n\)/g).length === 2){			
+				z[y - 1]?.match("#") &&  HeaderRewrite.push(z[y - 1]);
+let op = x.match(/\sresponse-header/) ?
+'http-response ' : '';
+     if(x.match(/\$1\$2/)){
+		  HeaderRewrite.push(x.replace(/(\^?http[^\s]+).+?n\)([^\:]+).+/,`${op}$1 header-del $2`))	
+		}else{
 				HeaderRewrite.push(
 					x.replace(
-						/(\^?http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+:\s(.+)\$2/,
-						"$1 header-replace-regex $2 $3 $4",
+						/(\^?http[^\s]+)[^\)]+\)([^:]+):([^\(]+).+\$1\s?\2?\:?([^\$]+)?\$2/,
+						`${op}$1 header-replace-regex $2 $3 $4''`,
 					),
 				);
+				}
+				}else{
+	$notification.post('不支持这条规则转换,已跳过','',`${x}`);
+				}
 				break;
 
 			case "echo-response":
@@ -80,7 +92,7 @@ body.forEach((x, y, z) => {
 					z[y - 1]?.match("#") && script.push(z[y - 1]);
 					script.push(
 						x.replace(
-							/(\^?http[^\s]+)\surl\s(response|request)-body\s(.+)\2-body(.+)/,
+							/([^\s]+)\surl\s(response|request)-body\s(.+)\2-body(.+)/,
 							`test = type=$2,pattern=$1,requires-body=1,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js, argument=$3->$4`,
 						),
 					);
@@ -89,13 +101,13 @@ body.forEach((x, y, z) => {
 	}
 }); //循环结束
 
-script = script.join("\n") && `[Script]\n${script.join("\n")}`;
+script = (script[0] || '') && `[Script]\n${script.join("\n")}`;
 
-URLRewrite = URLRewrite.join("\n") && `[URL Rewrite]\n${URLRewrite.join("\n")}`;
+URLRewrite = (URLRewrite[0] || '') && `[URL Rewrite]\n${URLRewrite.join("\n")}`;
 
-HeaderRewrite = HeaderRewrite.join("\n") && `[Header Rewrite]\n${HeaderRewrite.join("\n")}`;
+HeaderRewrite = (HeaderRewrite[0] || '') && `[Header Rewrite]\n${HeaderRewrite.join("\n")}`;
 
-MapLocal = MapLocal.join("\n") && `[MapLocal]\n${MapLocal.join("\n")}`;
+MapLocal = (MapLocal[0] || '') && `[MapLocal]\n${MapLocal.join("\n")}`;
 
 body = `${name}
 
@@ -103,12 +115,19 @@ ${script}
 ${URLRewrite}
 ${HeaderRewrite}
 ${MapLocal}
-${MITM}`;
+${MITM}`.replace(/\;/g,'#')
 
 
-console.log(body)
- $done({ response: { status: 200 ,body:body} });
-})();
+
+ $done({ response: { status: 200 ,body:body } });
+
+})()
+.catch((e) => {
+		$notification.post(`${e}`,'','');
+		$done()
+	})
+
+
 
 
 function http(req) {
