532888ee6943d7d0ce6161723c59f7b951d8b433
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-01 11:00:52 +0800

    更新 bdcloud.js

--- scripts/bdcloud.js
+++ scripts/bdcloud.js
@@ -5,7 +5,7 @@
 QuantumultX:
 
 [rewrite_local]
-https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user url script-response-body https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/scripts/bdcloud.js
 
 [mitm]
 hostname = pan.baidu.com
@@ -14,7 +14,7 @@ hostname = pan.baidu.com
 Surge4 or Loon:
 
 [Script]
-http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/BaiduCloud.js
+http-response https:\/\/pan\.baidu\.com\/rest\/\d\.\d\/membership\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/scripts/bdcloud.js
 
 [MITM]
 hostname = pan.baidu.com
