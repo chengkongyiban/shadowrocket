1336526bb4261e89dc5bd4510508df9ac4bc1f10
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-10 01:00:08 +0800

    更新 github-private-repo.js

--- scripts/github-private-repo.js
+++ scripts/github-private-repo.js
@@ -4,9 +4,8 @@
 🛎[配置说明]
 0️⃣ 
 1. 使用Surge模块:
-https://raw.githubusercontent.com/Peng-YM/QuanX/master/Rewrites/GithubPrivate/github-private-repo.sgmodule
-2. 使用Loon插件:
-https://raw.githubusercontent.com/Peng-YM/QuanX/master/Rewrites/GithubPrivate/github-private-repo.plugin
+https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/Block/github-private-repo.module
+
 1️⃣ 登陆Github > 点击头像下拉菜单 > 选择Settings > 左边菜单栏选择最后一个Developer settings > 选择Personal access tokens > Generate new token > Note里面填写token名字 > ☑️下面的勾选框选择第一项repo打钩（所有子项目自动勾选）> 点击Generate token按钮保存并返回。
 2️⃣ 在BoxJS里面填入用户名（打开Github，浏览器地址栏应该会显示https://github.com/这里是你的用户名/）和上面的token。
 🔘[QX无法使用的说明]
