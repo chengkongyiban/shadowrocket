e4a49dc98f043c17e17c7979b3c588632afde6c4
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-09 21:09:52 +0800

    更新 Loon_to_Shadowrocket.js

--- scripts/Loon_to_Shadowrocket.js
+++ scripts/Loon_to_Shadowrocket.js
@@ -5,25 +5,26 @@
    t&zd; = {  , }  花括号中的逗号
 
 ***************************/
+const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
+const isShadowrocket = 'undefined' !== typeof $rocket;
+const modStore = "https://loon-gallery.vercel.app/";
+
 var name = "";
 var desc = "";
 let req = $request.url.replace(/plugin/,'plugin');
 let urlArg = $request.url.replace(/.+plugin(\?.*)/,'$1');
-
-if (urlArg === ""){
+var original = [];//用于获取原文行号
+//获取参数
+var nName = urlArg.indexOf("n=") != -1 ? (urlArg.split("n=")[1].split("&")[0].split("+")) : null;
+var Pin0 = urlArg.indexOf("y=") != -1 ? (urlArg.split("y=")[1].split("&")[0].split("+")).map(decodeURIComponent) : null;
+var Pout0 = urlArg.indexOf("x=") != -1 ? (urlArg.split("x=")[1].split("&")[0].split("+")).map(decodeURIComponent) : null;
+//修改名字和简介
+if (nName === null){
 	name = req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1] || '无名';
-    desc = req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1] || '无名';
+    desc = name;
 }else{
-	if(urlArg.match("n=")){
-		name = urlArg.split("n=")[1].split("&")[0];
-	}else{
-		name = req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1] || '无名';
-	}
-	if(urlArg.match("d=")){
-		desc = urlArg.split("d=")[1].split("&")[0];
-	}else{
-		desc = name;
-	}
+	name = nName[0] != "" ? nName[0] : req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1];
+	desc = nName[1] != undefined ? nName[1] : nName[0];
 };
 name = "#!name=" + decodeURIComponent(name);
 desc = "#!desc=" + decodeURIComponent(desc);
@@ -65,8 +66,18 @@ body = `${plugin}`
 		.replace(/(#.+\n)\n/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 		.replace(/hostname\x20=\x20%APPEND%\x20\n\n安装失败\n\n1、请检查模块商店是否安装\n\n2、请检查是否开启HTTPS解密\n\n小火箭开启HTTPS解密教程https:\/\/t\.me\/h5683577\/3\n\nSurge开启HTTPS解密\(MITM\)教程https:\/\/t\.me\/h5683577\/135/,"hostname = %APPEND% \n\n模块商店已成功安装!!!")
-		
-		
+
+if (isSurgeiOS) {
+         if (body.match("验证结果↓↓↓") != null && body.match("请检查模块商店是否安装") == null)  {
+			 $notification.post("已成功安装模块商店","点击通知跳转模块商店","https://loon-gallery.vercel.app/",{url:modStore})
+		}else{};
+		}else{
+			if (body.match("验证结果↓↓↓") != null && body.match("请检查模块商店是否安装") == null)  {
+			 $notification.post("已成功安装模块商店","点击通知跳转模块商店","https://loon-gallery.vercel.app/",modStore)
+		}else{};
+		};				
+	
+
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
 
 })()
