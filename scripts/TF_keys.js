80a55eb1ba0211caa59cd37d3134afb2b15fccae
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-01-10 18:46:13 +0800

    更新 TF_keys.js

--- scripts/TF_keys.js
+++ scripts/TF_keys.js
@@ -4,7 +4,6 @@
 
 $persistentStore.write(null, 'request_id')
 let url = $request.url
-let fdbUrl = "https://t.me/zhangpeifu"
 let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
 
 let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
@@ -19,9 +18,9 @@ $persistentStore.write(session_digest, 'session_digest')
 $persistentStore.write(request_id, 'request_id')
 
 if (!$persistentStore.read('request_id') && $persistentStore.read('request_id') != 0) {
-	$notification.post('信息获取失败','为什么失败','我也不知道啊')
+	$notification.post('信息获取失败','请仔细阅读使用方法','不能说的秘密')
 console.log('request_id为' + $persistentStore.read('request_id'))
 } else {
-	$notification.post('请关闭本脚本', '信息获取成功','')
+	$notification.post('请关闭本脚本', '信息获取成功','最美不是下雨天')
 }
 $done({})
