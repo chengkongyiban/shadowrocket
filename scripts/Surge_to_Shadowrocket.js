a0cac15daaf055b012b58d4dcc739b9a46b89def
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-02-11 00:20:38 +0800

    更新 Surge_to_Shadowrocket.js

--- scripts/Surge_to_Shadowrocket.js
+++ scripts/Surge_to_Shadowrocket.js
@@ -5,36 +5,13 @@
    t&zd; = {  , }  花括号中的逗号
 
 ***************************/
-const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
-const isShadowrocket = 'undefined' !== typeof $rocket;
-var name = "";
-var desc = "";
 let req = $request.url.replace(/sg$/,'');
-let urlArg = $request.url.replace(/.+sg(\?.*)/,'$1');
-var original = [];//用于获取原文行号
-//获取参数
-var nName = urlArg.indexOf("n=") != -1 ? (urlArg.split("n=")[1].split("&")[0].split("+")) : null;
-var Pin0 = urlArg.indexOf("y=") != -1 ? (urlArg.split("y=")[1].split("&")[0].split("+")).map(decodeURIComponent) : null;
-var Pout0 = urlArg.indexOf("x=") != -1 ? (urlArg.split("x=")[1].split("&")[0].split("+")).map(decodeURIComponent) : null;
-//修改名字和简介
-if (nName === null){
-	name = req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1] || '无名';
-    desc = name;
-}else{
-	name = nName[0] != "" ? nName[0] : req.match(/.+\/(.+)\.(conf|js|snippet|txt)/)?.[1];
-	desc = nName[1] != undefined ? nName[1] : nName[0];
-};
-name = "#!name=" + decodeURIComponent(name);
-desc = "#!desc=" + decodeURIComponent(desc);
 
 !(async () => {
   let body = await http(req);
 //判断是否断网
-if(body == null){if(isSurgeiOS){
-	$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
- $done({ response: { status: 404 ,body:{} } });}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+if(body == null){$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
-}//识别客户端通知
 }else{//以下开始重写及脚本转换
 
 	body = body.match(/[^\r\n]+/g);
@@ -46,7 +23,7 @@ let URLRewrite = [];
 let script = [];
 
 body.forEach((x, y, z) => {
-	x = x.replace(/^ *(#|;|\/\/)/gi,'#');
+	x = x.replace(/^ *(#|;|\/\/)/g,'#');
 	
 	//判断注释
 	
@@ -61,25 +38,36 @@ body.forEach((x, y, z) => {
 //Mock转reject/request
 
 				let ptn = x.replace(/\x20{2,}/g," ").split(" data=")[0].replace(/^#|"/g,"");
-					let arg = x.split(' data="')[1].split('"')[0];
-					let fileName = arg.substring(arg.lastIndexOf('/') + 1);
-					let scname = arg.substring(arg.lastIndexOf('/') + 1, arg.lastIndexOf('.') );
+					let file = x.split(' data="')[1].split('"')[0];
+					let fileName = file.substring(file.lastIndexOf('/') + 1);
+					let scname = fileName.split(".")[0];
 					
 				if (fileName.match(/(img|dict|array|200|blank|tinygif)\.[^.]+$/i)){
+                
+                let mock2Reject
+                
+                if (fileName.match(/dict\.[^.]+$/i)){
+                    mock2Reject = "-dict";
+                    
+                }else if (fileName.match(/array\.[^.]+$/i)){
+                    mock2Reject = "-array";
+                    
+                }else if (fileName.match(/(200|blank)\.[^.]+$/i)){
+                    mock2Reject = "-200";
+                    
+                }else if (fileName.match(/(img|tinygif)\.[^.]+$/i)){
+                    mock2Reject = "-200";
+                };
+				
 				z[y - 1]?.match(/^#/) && URLRewrite.push(z[y - 1]);
-					
-				let mock2Dict = fileName.match(/dict\.[^.]+$/i) ? '-dict' : '';
-				let mock2Array = fileName.match(/array\.[^.]+$/i) ? '-array' : '';
-				let mock2200 = fileName.match(/(200|blank)\.[^.]+$/i) ? '-200' : '';
-				let mock2Img = fileName.match(/(img|tinygif)\.[^.]+$/i) ? '-img' : '';
+                
 				URLRewrite.push(
-						`${noteK}${ptn} - reject${mock2Dict}${mock2Array}${mock2200}${mock2Img}`
-				);
+						`${noteK}${ptn} - reject${mock2Reject}`)
 				}else{
 				z[y - 1]?.match(/^#/) && script.push(z[y - 1]);
 		
 		script.push(
-			`${noteK}${scname} = type=http-request,pattern=${ptn},script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/echo-response/index.js,argument=type=text/json&url=${arg}`)
+			`${noteK}${scname} = type=http-request,pattern=${ptn},script-path=https://raw.githubusercontent.com/xream/scripts/main/surge/modules/echo-response/index.js,argument=type=text/json&url=${file}`)
 				}
 				
 			}else{
@@ -123,7 +111,7 @@ body = `${mods}`
 		.replace(/t&zd;/g,',')
 		.replace(/\[Map\x20?Local\]/gi,'')
 		.replace(/undefined$/,'')
-		.replace(/(#.+\n)\n+/g,'$1')
+		.replace(/(#.+\n)\n+(?!\[)/g,'$1')
 		.replace(/\n{2,}/g,'\n\n')
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
