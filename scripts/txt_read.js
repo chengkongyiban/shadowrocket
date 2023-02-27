e02ad176fa6f80e684b2af83aae9dcc4b561abba
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-02-27 21:59:50 +0800

    更新 txt_read.js

--- scripts/txt_read.js
+++ scripts/txt_read.js
@@ -20,8 +20,8 @@ console.log(req)
   let body = await http(req);
 //判断是否断网
 if(body == null){if(isSurgeiOS || isStashiOS){
-	$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
- $done({ response: { status: 404 ,body:{} } });}else{$notification.post("重写转换：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+	$notification.post("文本预览：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
+ $done({ response: { status: 404 ,body:{} } });}else{$notification.post("文本预览：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
 }//识别客户端通知
 }else{
