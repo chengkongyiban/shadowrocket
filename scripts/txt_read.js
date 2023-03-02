8263496c8ec9cb66085f2bd2c5ed20ee84d09f0e
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-03-02 15:24:39 +0800

    更新 txt_read.js

--- scripts/txt_read.js
+++ scripts/txt_read.js
@@ -6,27 +6,14 @@
 
 [MITM]
 
-hostname = %APPEND% github.com, raw.githubusercontent.com,gitlab.com,gist.githubusercontent.com
+hostname = %APPEND% github.com, raw.githubusercontent.com,gitlab.com,gist.githubusercontent.com,gitlab.com
 ****************************/
-
-const isStashiOS = 'undefined' !== typeof $environment && $environment['stash-version'];
-const isSurgeiOS = 'undefined' !== typeof $environment && $environment['surge-version'];
-const isShadowrocket = 'undefined' !== typeof $rocket;
-const isLooniOS = 'undefined' != typeof $loon;
 	
 let req = $request.url.replace(/\.t_read\.txt$/,'');
 !(async () => {
   let body = await http(req);
-//判断是否断网
-if(body == null){if(isSurgeiOS || isStashiOS){
-	$notification.post("文本预览：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈",{url:"https://t.me/zhangpeifu"})
- $done({ response: { status: 404 ,body:{} } });}else{$notification.post("文本预览：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
- $done({ response: { status: 404 ,body:{} } });
-}//识别客户端通知
-}else{
 
  $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
-}//判断是否断网的反括号
 
 })()
 .catch((e) => {
