9feea57139790e9237aa87590047b663d3c66625
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-06-05 09:43:46 +0800

    更新 Loon_to_Shadowrocket.js

--- scripts/Loon_to_Shadowrocket.js
+++ scripts/Loon_to_Shadowrocket.js
@@ -13,7 +13,7 @@ let req = $request.url;
 !(async () => {
   let body = await http(req);
 //判断是否断网
-if(body == null){$notification.post("模块商店：未获取到body","请检查网络及节点是否畅通","认为是bug?点击通知反馈","https://t.me/zhangpeifu")
+if(body == null){$notification.post("模块商店：未获取到body","请检查网络及节点是否畅通\n" + "源链接为" + $request.url,"认为是bug?点击通知反馈","https://t.me/zhangpeifu")
  $done({ response: { status: 404 ,body:{} } });
 }else{//以下开始重写及脚本转换
 
