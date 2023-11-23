82a1b2d5579df02f9f2514c548860f6981ea05ff
chengkongyiban <48396581+chengkongyiban@users.noreply.github.com>
2023-11-24 01:42:56 +0800

    更新 txt_read.js

--- scripts/txt_read.js
+++ scripts/txt_read.js
@@ -2,18 +2,26 @@
 主要用于预览yaml及snippet 等Safari需要下载的文本
 在需要预览的链接末尾加上.t_read.txt
 [Script]
-文本预览器 = type=http-request,pattern=\.t_read\.txt$,requires-body=1,max-size=3145728,script-path=https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/scripts/txt_read.js, script-update-interval=0
+文本预览器 = type=http-request, pattern=\.t_read\.txt$, requires-body=false, script-path=https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/scripts/txt_read.js, script-update-interval=0
 
 [MITM]
 
 hostname = %APPEND% github.com, raw.githubusercontent.com,gitlab.com,gist.githubusercontent.com,gitlab.com
+
+---------------------------
+[rewrite_local]
+
+\.t_read\.txt$ url script-analyze-echo-response https://raw.githubusercontent.com/chengkongyiban/shadowrocket/main/scripts/txt_read.js 
+
 ****************************/
+const isQX = "undefined" != typeof $task;
 
 let req = $request.url.replace(/\.t_read\.txt$/,'');
+
 !(async () => {
   let body = await http(req);
 
- $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
+isQX ? $done({  status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } ) : $done({ response: { status: 200 ,body:body ,headers: {'Content-Type': 'text/plain; charset=utf-8'} } });
 
 })()
 .catch((e) => {
